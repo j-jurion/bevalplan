6d86ec284e272bed2e94c0add6a8a77e2112c274
Joeri <51537901+j-jurion@users.noreply.github.com>
2025-10-21 21:57:08 +0200

    removed key prop

--- bevalplan/src/components/Cards/Tile.jsx
+++ bevalplan/src/components/Cards/Tile.jsx
@@ -1,6 +1,6 @@
 export default function Tile({ option }) {
     return (
-        <div className="icon-toggle-container mb-3" key={option.id}>
+        <div className="icon-toggle-container mb-3">
             <input type="checkbox" id={option.id} className="icon-toggle-input" />
             <label htmlFor={option.id} className="icon-toggle-label d-flex flex-column align-items-center justify-content-center p-2">
                 <img src={option.image} className="icoontje" alt={option.alt} /> 
