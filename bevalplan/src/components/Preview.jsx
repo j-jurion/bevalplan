ffc487ad00c4fb287ab2882fe8e58fb545690979
Joeri <51537901+j-jurion@users.noreply.github.com>
2025-10-21 20:46:05 +0200

    added griddragfield

--- bevalplan/src/components/Preview.jsx
+++ bevalplan/src/components/Preview.jsx
@@ -1,10 +1,14 @@
-import MostImportant from "./MostImportant";
-import SelectionList from "./SelectionList";
+import GridDragField from "./GridDragField";
 
 export default function Preview() {
-  return (
+return (
     <div className="col-12 col-lg-8 preview-column">
-        <h1>Preview Component</h1>
+        <div className="a4-wrapper">
+            <div className="a4-document p-5 shadow">
+                <h2 id="liveNaamTitel" className="text-center mb-5">Geboorteplan Naam + Naam Partner</h2>
+                <GridDragField />
+            </div>
+        </div>
     </div>
-  );
+    );
 };
